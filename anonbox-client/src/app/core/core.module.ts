cbeb1bc6421e88b7e620660f1fcd0cd1a002ce2f
andyalam <andyalam@hotmail.com>
2017-05-14 17:26:57 -0700

    core module, nav component

---
+++ anonbox-client/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+	declarations: [
+		NavComponent
+	],
+	imports: [],
+	exports: [
+		NavComponent
+	],
+	providers: []
+})
+export class CoreModule {}
