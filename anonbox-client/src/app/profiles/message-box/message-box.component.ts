70dde734fe0323532889195bf4402f120b70e231
andyalam <andyalam@hotmail.com>
2017-09-22 18:50:32 -0700

    message box input property setters and focus

--- anonbox-client/src/app/profiles/message-box/message-box.component.ts
+++ anonbox-client/src/app/profiles/message-box/message-box.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 import { ProfilesService } from '../profiles.service';
@@ -8,27 +15,49 @@ import { ProfilesService } from '../profiles.service';
   templateUrl: './message-box.component.html',
   styleUrls: ['./message-box.component.scss']
 })
-export class MessageBoxComponent implements OnInit {
+export class MessageBoxComponent implements OnInit, AfterViewInit {
 
   messageForm: FormGroup;
-  @Input() receiver;
-  @Input() sender;
-  @Input() boxType;
+
+  private _receiver;
+  private _sender;
+  private _boxType;
+
+  @Input() set receiver(value) {
+    this._receiver = value;
+    this.focusMessageBox();
+  };
+
+  @Input() set sender(value) {
+    this._sender = value;
+    this.focusMessageBox();
+  };
+
+  @Input() set boxType(value) {
+    this._boxType = value;
+    this.focusMessageBox();
+  };
 
   isFormShown: boolean = true;
   isFormLoading: boolean;
   isSuccessShown: boolean;
   errorMessage: string;
 
+  @ViewChild('input') input;
+
   constructor(private profilesService: ProfilesService) {
     this.initMessageForm();
   }
 
   ngOnInit() {
-    if (!this.receiver) {
+    if (!this._receiver) {
       this.errorMessage = 'Message Box Failed to Initialize!';
       this.isFormShown = false;
     }
+  }
+
+  ngAfterViewInit() {
+    this.focusMessageBox();
   }
 
   initMessageForm() {
@@ -37,14 +66,18 @@ export class MessageBoxComponent implements OnInit {
     });
   }
 
+  focusMessageBox() {
+    this.input && this.input.nativeElement.focus();
+  }
+
   onSubmitMessageForm() {
     this.isFormLoading = true;
 
     this.profilesService
       .postMessage(
-        this.receiver,
+        this._receiver,
         this.messageForm.value.message,
-        this.boxType
+        this._boxType
       )
       .subscribe(
         this.handleMessageSuccess.bind(this),
