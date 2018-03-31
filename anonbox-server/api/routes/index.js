6efe8f807f7e8850df6a76685975c588949027c4
andyalam <andyalam@hotmail.com>
2018-03-30 22:29:11 -0700

    upgraded packages on api, experimenting with some auth things

--- anonbox-server/api/routes/index.js
+++ anonbox-server/api/routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
+const boxController = require('../controllers/boxController');
+const userController = require('../controllers/userController');
+const { catchErrors } = require('../handlers/handlers');
 const jwt = require('express-jwt');
 // pass auth to any routes that require the user to be authenticated.
 const auth = jwt({
   secret: process.env.JWT_SECRET,
   userProperty: 'payload'
 });
-const authController = require('../controllers/authController');
-const boxController = require('../controllers/boxController');
-const userController = require('../controllers/userController');
-const { catchErrors } = require('../handlers/handlers');
 
 // Authentication
 router.post('/register', catchErrors(authController.register));
@@ -18,11 +18,11 @@ router.post('/login', authController.login);
 // Boxes & Profile pages
 router.get('/profile/:username', catchErrors(boxController.getProfile));
 router.post('/profile/:username', catchErrors(boxController.postMessage));
-router.post('/box', catchErrors(boxController.createBox));
+router.post('/box', auth, catchErrors(boxController.createBox));
 router.delete('/box/:boxType', catchErrors(boxController.deleteBox));
 
 // Image handling
-router.post('/profile/:username/profile-image', catchErrors(userController.postImage));
+router.post('/profile/:username/profile-image', auth, catchErrors(userController.postImage));
 router.delete('/profile/:username/profile-image', catchErrors(userController.deleteImage));
 
 module.exports = router;
