d52e5bec6d87307d6065ecdf5f85817a9067cc41
andyalam <andyalam@hotmail.com>
2018-04-26 09:39:40 -0700

    pipe tested

--- anonbox-client/src/app/shared/pipes/api-error.pipe.spec.ts
+++ anonbox-client/src/app/shared/pipes/api-error.pipe.spec.ts
@@ -1,8 +1,39 @@
 import { ApiErrorPipe } from './api-error.pipe';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('ApiErrorPipe', () => {
+  let pipe;
+
+  beforeEach(() => {
+    pipe = new ApiErrorPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new ApiErrorPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should parse HttpErrorResponse correctly', () => {
+    const httpErrorResponse = new HttpErrorResponse({
+      error: {
+        error: 'my error'
+      }
+    });
+    expect(pipe.transform(httpErrorResponse)).toEqual('my error');
+  });
+
+  it('should parse anything that isn\'t HttpErrorResponse correctly', () => {
+    const [
+      t1,
+      t2,
+      t3
+    ] = [
+      pipe.transform('test'),
+      pipe.transform(5),
+      pipe.transform({ key: 'anything' })
+    ];
+
+    expect(t1).toEqual('test');
+    expect(t2).toEqual(5);
+    expect(t3).toEqual({ key: 'anything' });
   });
 });
